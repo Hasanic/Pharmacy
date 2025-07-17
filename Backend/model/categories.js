1975fbe7fa2ab0158bd6156792787a3088268a30
Hassan Mohamed <hassanicmo@gmail.com>
2025-07-17 18:32:00 +0300

    categories

--- Backend/model/categories.js
+++ Backend/model/categories.js
@@ -6,12 +6,12 @@ const categoriesSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true, 
-      unique: true, 
+      required: true,
+      unique: true,
     },
     user_id: {
       type: mongoose.Schema.ObjectId,
-      default:null
+      default: null,
     },
     unique_id: Number,
   },
@@ -19,7 +19,7 @@ const categoriesSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
+//hassan
 
 
 categoriesSchema.plugin(AutoIncrement, {
